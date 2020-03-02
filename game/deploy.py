3b790a0ecc7d8247bee90e4b697cfb9dce2bf20b
william <191marquis@gmail.com>
2020-03-02 15:09:39 -0500

    [BUGFIX] Eliminate caching for flask dev deployment

--- game/deploy.py
+++ game/deploy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from flask import Flask, render_template
 app = Flask(__name__)
-
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/', strict_slashes=False)
 def deploy():
