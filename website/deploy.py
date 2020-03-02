7ccb11a04f76494359d7ea1023ab98dbb0fb3f61
william <191marquis@gmail.com>
2020-03-02 14:49:30 -0500

    [INIT] Add deployment structure for website

---
+++ website/deploy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def deploy():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
+
