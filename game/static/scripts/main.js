91029c064a79955e5f1ab193999ac1f3365be13e
william <191marquis@gmail.com>
2020-03-02 21:34:47 -0500

    [FEATURE] Add sprites

--- game/static/scripts/main.js
+++ game/static/scripts/main.js
@@ -33,7 +33,7 @@ var config = {
     }
 };
 
-var ship;
+var ships = [];
 var cursors;
 // configuration is passed into the Game obj this will start the process of bring the game to life
 var game = new Phaser.Game(config);
@@ -41,27 +41,39 @@ var game = new Phaser.Game(config);
 function preload ()
 {
     this.load.image('sky', 'assets/download.png');
-    this.load.spritesheet('ship', 
-        'assets/sprite7.png',
-        { frameWidth: 64, frameHeight: 48 }
-    );
-
+    this.load.image('player1', 'static/assets/images/whiteship.png');
+    this.load.image('player2', 'static/assets/images/greenship.png');
+    this.load.image('player3', 'static/assets/images/orangeship.png');
 }
 
 function create ()
 {
     this.add.image(400, 300, 'sky');
 
-    ship = this.physics.add.sprite(100, 450, 'ship');
-
+    let ship = this.physics.add.sprite(100, 450, 'player1');
     ship.setBounce(0.2);
     ship.setCollideWorldBounds(true);
-    ship = new Ship(ship, 100, 450, 1, 0);
+    ships.push(ship)
+
+    ship = this.physics.add.sprite(200, 500, 'player2');
+    ship.setBounce(0.2);
+    ship.setCollideWorldBounds(true);
+    ships.push(ship)
+
+    ship = this.physics.add.sprite(400, 200, 'player3');
+    ship.setBounce(0.2);
+    ship.setCollideWorldBounds(true);
+    ships.push(ship)
+
+    ships[0] = new Ship(ships[0], 100, 450, 1, 0);
+    ships[1] = new Ship(ships[1], 200, 500, 1, 0);
+    ships[2] = new Ship(ships[2], 400, 200, 1, 0);
 }
 
 function update ()
 {
-    cursors = this.input.keyboard.createCursorKeys();
+    let cursors = this.input.keyboard.createCursorKeys();
+    let kb = this.input.keyboard
     let left, right, up, down = false
     if (cursors.left.isDown) {
         left = true;
@@ -74,5 +86,27 @@ function update ()
     } else if (cursors.up.isDown) {
         up = true
     }
-    ship.update(false, false, up, down, right, left, false, false);
+    ships[0].update(false, false, up, down, right, left, false, false);
+
+    let w = this.input.keyboard.addKey('W');
+    let a = this.input.keyboard.addKey('A');
+    let s = this.input.keyboard.addKey('S');
+    let d = this.input.keyboard.addKey('D');
+
+    left = false;
+    right = false;
+    up = false;
+    down = false;
+    if (a.isDown) {
+        left = true;
+    } else if (d.isDown) {
+        right = true;
+    }
+
+    if (s.isDown) {
+        down = true
+    } else if (w.isDown) {
+        up = true
+    }
+    ships[1].update(false, false, up, down, right, left, false, false);
 }
